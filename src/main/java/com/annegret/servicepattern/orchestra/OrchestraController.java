1ee218953454dad1b76fe7f4809e7c16eaa909cf
Grinseteddy <annegret.kampe@arcor.de>
2019-03-26 11:45:30 +0100

    Initial push to repository

---
+++ src/main/java/com/annegret/servicepattern/orchestra/OrchestraController.java
@@ -0,0 +1,84 @@
+package com.annegret.servicepattern.orchestra;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class OrchestraController {
+    @RequestMapping(method = RequestMethod.GET, value ="process")
+    public String processed(@RequestParam("inputString") String inputString) throws IOException {
+        return " process:"+ process(inputString);
+    }
+
+    private String process(String inputString) throws IOException {
+        if (inputString.length()>=0) {
+            try {
+                String mappedString = mapping(inputString);
+                String filteredString = filter(mappedString);
+                return filteredString;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return  "";
+
+    }
+
+    private String mapping(String inputString) throws IOException {
+        if (inputString.length() > 0) {
+
+            return callService("http://localhost:8082/mapping", "inputString", inputString);
+
+
+        }
+        return "";
+    }
+
+    private String filter(String inputString) throws IOException {
+        if (inputString.length() > 0) {
+
+            return callService("http://localhost:8083/filter", "inputString", inputString);
+
+
+        }
+        return "";
+
+    }
+
+    private String callService(String urlAsString, String key, String parameter)  throws IOException {
+        try {
+            String urlParameter = urlAsString+"?"+key+"="+parameter;
+            URL url=new URL(urlParameter);
+            HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            if (conn.getResponseCode() != 200) {
+                return String.valueOf(conn.getResponseCode());
+                //use http response enum
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+
+            output = br.readLine();
+            //think about timeouts
+            conn.disconnect();
+
+            return output;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+}
---
+++ src/test/java/com/annegret/servicepattern/orchestra/OrchestraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.annegret.servicepattern.orchestra;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrchestraApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
